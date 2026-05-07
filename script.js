b14eb3985e5827bb9d75e819523283e5ba5938c9
MUTHENYA00 <brianmunene131@gmail.com>
2026-05-07 13:48:06 +0300

    Updated Some Files

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 let currentIndex = 0;
 
-function initSlider() {
+document.addEventListener("DOMContentLoaded", function () {
+
     const images = document.querySelectorAll(".header-img");
+    const nextBtn = document.querySelector(".arrow.right");
+    const prevBtn = document.querySelector(".arrow.left");
 
     function showImage(index) {
         images.forEach(img => img.classList.remove("active"));
         images[index].classList.add("active");
     }
 
-    // make functions global so HTML onclick can access them
-    window.nextImage = function () {
+    function nextImage() {
         currentIndex = (currentIndex + 1) % images.length;
         showImage(currentIndex);
-    };
+    }
 
-    window.prevImage = function () {
+    function prevImage() {
         currentIndex = (currentIndex - 1 + images.length) % images.length;
         showImage(currentIndex);
-    };
+    }
 
-    // auto slide
-    setInterval(function () {
-        window.nextImage();
-    }, 3000);
-}
+    // 👉 button clicks now WORK properly
+    nextBtn.addEventListener("click", nextImage);
+    prevBtn.addEventListener("click", prevImage);
 
-// run after page loads
-document.addEventListener("DOMContentLoaded", initSlider);
+    // 👉 auto slide
+    setInterval(nextImage, 3000);
+
+});
