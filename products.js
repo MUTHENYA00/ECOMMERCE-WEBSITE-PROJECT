dd703062845f993e18e5c96d9a21c134f80aa20a
MUTHENYA00 <brianmunene131@gmail.com>
2026-05-12 15:29:11 +0300

    made changes in product.js

--- products.js
+++ products.js
@@ -1,13 +1,21 @@
-console.log("JS is connected");
+document.addEventListener("DOMContentLoaded", () => {
 
-document.querySelectorAll(".add-to-cart").forEach(button => {
-    button.addEventListener("click", function () {
-        window.location.href = "cart.html";
+    
+    // ADD TO CART (basic redirect)
+    
+    document.querySelectorAll(".add-to-cart").forEach(button => {
+        button.addEventListener("click", function () {
+            window.location.href = "cart.html";
+        });
     });
+
 });
 
 
+
+// OPEN BUY NOW MODAL
 function openBuyNow(button){
+
     let product = button.closest(".Product-Card");
 
     if (!product) {
@@ -15,21 +23,43 @@ function openBuyNow(button){
         return;
     }
 
-    let name = product.getAttribute("data-name");
-    let price = product.getAttribute("data-price");
-    let image = product.getAttribute("data-image");
+    let name = product.getAttribute("data-name") || "";
+    let price = product.getAttribute("data-price") || "";
+    let image = product.getAttribute("data-image") || "";
 
-    document.getElementById("modalName").innerText = name || "";
-    document.getElementById("modalPrice").innerText = price || "";
-    document.getElementById("modalImage").src = image || "";
+    let modalName = document.getElementById("modalName");
+    let modalPrice = document.getElementById("modalPrice");
+    let modalImage = document.getElementById("modalImage");
+    let modal = document.getElementById("buyNowModal");
 
-    document.getElementById("buyNowModal").style.display = "block";
+    if (modalName) modalName.innerText = name;
+    if (modalPrice) modalPrice.innerText = price;
+    if (modalImage) modalImage.src = image;
+
+    if (modal) {
+        modal.style.display = "flex";
+    }
 }
 
+
+
+// CLOSE MODAL
+
 function closeBuyNow(){
+
     let modal = document.getElementById("buyNowModal");
 
     if (modal) {
         modal.style.display = "none";
     }
+}
+
+
+// CONFIRM PURCHASE
+
+function confirmPurchase(){
+
+    alert("Purchase Confirmed!");
+
+    closeBuyNow();
 }
